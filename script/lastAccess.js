4767d3da4771c63f2df53c2526f1d3b4799348b1
pedrovinhas <pstvideos@gmail.com>
2023-03-26 21:29:56 -0300

    feat: add GET function to fetch php api

---
+++ script/lastAccess.js
@@ -0,0 +1,40 @@
+const API_URL = 'http://localhost/api/';
+
+function getLastAccess() {
+axios.get(API_URL)
+  .then(response => {
+    const data = response.data;
+    const lastAccessDate = data.last_access;
+    const country = data.country;
+
+    const parsedDate = new Date(lastAccessDate)
+
+    const formattedDate = parsedDate.toLocaleDateString('pt-BR', {
+      year: 'numeric',
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit'
+    })
+  
+    const footer = document.querySelector('footer');
+    const footerContent = 
+      `
+      <span class='footer__text '>
+        Último acesso em <span class='footer__highlight'> ${formattedDate} </span>
+      </span>
+      <span class='footer__text'>
+       às informações do país <span class='footer__country'>  ${country} </span>
+      </span>
+      `
+
+      footer.innerHTML = footerContent
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+}
+
+
+window.addEventListener('load', getLastAccess())
